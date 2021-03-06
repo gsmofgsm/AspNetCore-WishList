084551b0e525df7095e67330d8b0871c37ca1010
Qing Ma <gsmofgsm@hotmail.com>
2021-03-06 23:37:20 +0100

    fix 6.5 delete item

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -40,7 +40,7 @@ namespace WishList.Controllers
         public IActionResult Delete(int Id)
         {
             var item = _context.Items.FirstOrDefault(i => i.Id == Id);
-            _context.Remove(item);
+            _context.Items.Remove(item);
             _context.SaveChanges();
             return RedirectToAction("Index");
         }
